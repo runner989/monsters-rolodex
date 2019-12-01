78fbb28d4800ecac4f36e9cf3c2f9cfcd6d204e1
runner989 <runner989@hotmail.com>
2019-11-30 20:52:30 -0500

    Updates

---
+++ precache-manifest.b59c1a472363ed571d618131975c45cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073820580891271f2b1035eb9df822ec",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "09877152438b51d79f71",
+    "url": "/monsters-rolodex/static/css/main.01faccf0.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "09877152438b51d79f71",
+    "url": "/monsters-rolodex/static/js/main.65bbfe13.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
